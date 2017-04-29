6f42e888b6d7787c757f64fe3d0d9021eef391bb
Rob Rose <bertra97@gmail.com>
2017-04-29 23:14:29 +0000

    Added stuff for getting the images
    - Wrote most of the app.py file that returns the url to an S3 file. Not
      sure if it works entirely but I hope so.
    - All you do is to submit a POST request to the /img endpoint of the
      ngrok URL.
    - Couldn't figure out how to make the config file stay around while not
      being updated so I think it got removed. Check older commits for the
      format.

--- img-capture/app.py
+++ img-capture/app.py
@@ -1,23 +1,52 @@
 from flask import Flask, render_template, flash
-from flask_wtf import Form
-from flask_wtf.file import FileField
-from tools import s3_upload
+import boto
+from boto.s3.key import Key
+import uuid
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 lastUploadedFile = 0
 
-def take_photo():
+def take_photo(fileName=0):
     return 
 
 @app.route('/img', methods=['POST', 'GET'])
 def upload_img():
-    
-    if form.validate_on_submit():
-        output = s3_upload(form.example)
-        flash('{src} uploaded to S3 as {dst}'.format(src=form.example.data.filename, dst=output))
-    return render_template('example.html', form=form)
+    if request.method == 'POST':
+        
+        # Create connection to Amazon S3
+        s3 = boto.s3.connect_s3(app.config["S3_KEY"], app.config["S3_SECRET"])
+
+        # Create a link to the bucket.
+        bucket = s3.get_bucket(app.config["S3_BUCKET"])
+
+        # A unique ID representing the photot
+        fileId = uuid4().hex
+
+        # The file name
+        fileName = fileId + '.jpeg'
+        
+        # File address
+        fileNameWithDir = app.config["S3_UPLOAD_DIRECTORY"] + "/" + fileName  
+
+        #  This is where it calls a function to take a photo using the webcam
+        img_file = take_photo(fileName)
+        
+        # Default
+        toReturn = "ERROR"
+
+
+        if img_file != None:
+            k = Key(bucket)
+            k.key = img_file
+            k.set_contents_from_filename(img_file)
+            toReturn = fileNameWithDir
+
+        return str(fileNameWithDir)
+
+    return "ERROR"
+
 
 
 if __name__ == '__main__':
