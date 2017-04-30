59a234a8b62071fe36c86ac4602ea94271f208ff
Rob Rose <bertra97@gmail.com>
2017-04-30 10:03:23 +0000

    Worked on ReadIntent and added session variables
    - Added more code to ReadIntent, I think it should work now, once we get
      the Google Code fully functional.
    - Added some of the templates to the yaml file.
    - Added an Enum to the constants.py file to help save session state. I'm
      not sure if that'll be useful though.

---
+++ alexa/constants.py
@@ -0,0 +1,7 @@
+from enum34 import Enum
+
+AskTasks = Enum("AskTasks", "read desc summ")
+
+CURR_TASK = "CURR_TASK"
+GOOG_SMALL = "google_ai_read_small"
+GOOG_LARGE = "google_ai_read_small"
--- alexa/eyelexa.py
+++ alexa/eyelexa.py
@@ -12,6 +12,7 @@ import Algorithmia
 
 # My very own files.
 import constants
+from constants import AskTasks 
 import getters
 
 app = Flask(__name__)
@@ -21,65 +22,72 @@ logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 @ask.launch
 def launch():
-    
-
-    return
+    card_title = render_template('card_title')
+    return statement("This is the launch statement")
 
 @ask.intent("AMAZON.CancelIntent")
 def cancelIntent():
-
-    return
+    return statement("This is the cancel intent")
 
 @ask.intent("AMAZON.HelpIntent")
 def helpIntent():
-
-    return
+    return statement("This is the help intent")
 
 
 @ask.intent("AMAZON.StopIntent")
 def stopIntent():
-
-    return
+    return statement("This is the stop intent")
 
 
 @ask.intent("AMAZON.YesIntent")
 def yesIntent():
-
-    return
+    return statement("This is the yes intent")
 
 @ask.intent("AMAZON.NoIntent")
 def noIntent():
-
-    return
+    return statement("This is the no intent")
 
 
 @ask.intent("DescribeIntent")
 def describeIntent():
-
-    
+    session.attributes[constants.CURR_TASK] = AskTasks.desc
     return
 
 
 @ask.intent("ReadIntent")
 def readIntent():
+    session.attributes[constants.CURR_TASK] = AskTasks.read
     
-    photoUrl = getPhoto()
+    card_title = render_template('card_title')
+
+    photoUrl = getters.getPhoto()
 
     if(photoUrl == ""):
-        return statement(
-                "I encountered an error while checking for your image. ")
+        # This isn't as much of an error as it is a just a lack of text
+        err_msg = render_template('err_no_txt')
+        return statement(err_msg)
+    # These two are general errors that might be triggered by different
+    # things happening. They aren't very helpful, this is a hackathon.
     elif(photoUrl == "IMG_ERROR"):
-        return statement(
-                "I encountered an error while retrieving your image."
+        err_msg = render_template('err_img')
+        return statement(err_msg)
     elif(photoUrl == "AUTH_ERROR"):
-        return statement(
-                "I encountered an error while requesting your image.")
+        err_msg = render_template('err_auth')
+        return statement(err_msg)
     else:
-        googleReply = sendToGoogle(photoUrl)
-         
-        return ""
-
-@ask.intent("SummarizeIntent")
-def summarizeIntent():
-    
-
+       `
+        # Send our image to Google and get the text back
+        googleReply = getters.sendToGoogle(photoUrl)
+        
+        # If a lot of text is returned, we'll only take the first tweet and a
+        # half. We send all of it to the Alexa app though.
+        if(len(googleReply) > 210):
+            message = render_template(
+                    constants.GOOG_LARGE, text=googleReply[:210])
+        else:
+            message = render_template(
+                    constants.GOOG_SMALL, text=googleReply)
+        
+        # Return the statement and send the thing to the card.
+        return statement(message).simple_card(card_title, googleReply)
+ 
