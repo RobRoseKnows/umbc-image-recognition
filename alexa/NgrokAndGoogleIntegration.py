b1a07ba62477791cc190932343a4571beb62988e
michris1 <michris1@umbc.edu>
2017-04-30 07:15:47 -0400

    Completed Ngrok and Google Vision testing

--- alexa/NgrokAndGoogleIntegration.py
+++ alexa/NgrokAndGoogleIntegration.py
@@ -8,10 +8,11 @@ import urllib2
 import os
 
 
-endpoint_url = "https://omh5gu7ow3.execute-api.us-east-2.amazonaws.com/dev/processmessage"
+endpoint_url = os.environ["ENDPOINT_URL"]
 def getImageText(url):
 
 
+    print (url)
     try:
         data = {"inputMessage": {
                     "Url": url
@@ -23,10 +24,10 @@ def getImageText(url):
         
         request = urllib2.Request(endpoint_url, data = json.dumps(data), headers = headers)
         response = urllib2.urlopen(request)
-        
-        print('Response text: {} \nResponse status: {}'.format(response.read(), response.getcode()))
+        toReturn = response.read()
+        print('Response text: {} \nResponse status: {}'.format(toReturn, response.getcode()))
 
-        return response.read()
+        return toReturn
     except Exception as e:
         print(e)
 
@@ -46,7 +47,6 @@ def getPhoto():
         request = urllib2.Request(os.environ["NGROK_URL"], data = json.dumps(data), headers = headers)
         response = urllib2.urlopen(request)
         
-        print(response.read())
         return response.read()
         
     except Exception as e:
@@ -55,4 +55,65 @@ def getPhoto():
         raise e
 
 def lambda_handler(event, context):
-    return getImageText(getPhoto())
+    string = getImageText(getPhoto())
+    print (string)
+    return stringfrom __future__ import print_function
+
+import json
+import urllib
+import boto3
+import urllib
+import urllib2
+import os
+
+
+endpoint_url = os.environ["ENDPOINT_URL"]
+def getImageText(url):
+
+
+    print (url)
+    try:
+        data = {"inputMessage": {
+                    "Url": url
+            }
+        }
+
+        headers = {"Content-Type": "application/json"
+        }
+        
+        request = urllib2.Request(endpoint_url, data = json.dumps(data), headers = headers)
+        response = urllib2.urlopen(request)
+        toReturn = response.read()
+        print('Response text: {} \nResponse status: {}'.format(toReturn, response.getcode()))
+
+        return toReturn
+    except Exception as e:
+        print(e)
+
+# Calls the laptop using ngrok and then 
+# Returns: a URL to the photo in S3.
+def getPhoto():
+    try:
+        
+        data = {"inputMessage": {
+                    "key": os.environ["NGROK_AUTH_KEY"]
+            }
+        }
+
+        headers = {"Content-Type": "application/json",
+        }
+        # Invoke Cloud Endpoints API
+        request = urllib2.Request(os.environ["NGROK_URL"], data = json.dumps(data), headers = headers)
+        response = urllib2.urlopen(request)
+        
+        return response.read()
+        
+    except Exception as e:
+        print(e)
+        print('Error in retrieving image from computer')
+        raise e
+
+def lambda_handler(event, context):
+    string = getImageText(getPhoto())
+    print (string)
+    return string
